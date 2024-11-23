cb8d54d24fd80a9415f2e9197ef35f8f9478d89c
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2024-11-24 01:04:55 +0600

    scripts update

--- scripts.js
+++ scripts.js
@@ -10,3 +10,26 @@ function login(event) {
     alert("Signed up successfully!");
   }
   
+
+  /* Disable Right Click*/
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+/*Disable Keyboard Shortcuts (F12, Ctrl+Shift+I, Ctrl+U)*/
+document.addEventListener('keydown', function(e) {
+  // Disable F12 (Inspect)
+  if (e.keyCode === 123) {
+    e.preventDefault();
+  }
+  // Disable Ctrl+Shift+I (Inspect)
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+    e.preventDefault();
+  }
+  // Disable Ctrl+U (View Source)
+  if (e.ctrlKey && e.keyCode === 85) {
+    e.preventDefault();
+  }
+});
+function goBack() {
+  window.history.back(); /* Navigates back to the previous page */
+}
